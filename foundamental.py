e5ab00f29e197638d40a9b4c7c4cb0afea02170b
xcodej <xcoderj@gmail.com>
2017-03-31 12:16:06 +0800

    python foundamental

---
+++ foundamental.py
@@ -0,0 +1,32 @@
+print('''line1
+line2
+line3''')
+print(3 > 2)
+
+print(True or False)
+print(True and True)
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+# '//' 地板除
+print(10 // 3)
+print(10 / 3)
+print(10 % 3)
+
+n = 123
+f = 456.789
+s1 = 'Hello, world'
+s2 = 'Hello, \'Adam\''
+s3 = r'Hello, "Bart"'
+s4 = r'''Hello,
+Lisa!'''
+s5 = '''Hello,
+Lisa!'''
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
+print(s5)
