429e7a6c9754c072580b9dd1d0385e38f08c2f0f
xcodej <xcoderj@gmail.com>
2017-04-02 23:25:06 +0800

    conditions

---
+++ conditions.py
@@ -0,0 +1,14 @@
+age = 20
+if age >= 18:
+	print('your age is', age)
+	print('adult')
+elif age>= 6:
+	print('teenager')
+else:
+	print('kid')
+
+birth = int(input('birth: '))
+if birth < 2000:
+	print('before 00')
+else:
+	print('after 00')
