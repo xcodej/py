dd94d00d3b91f02370b8eb45ad3c7c15bdc9a707
xcodej <xcoderj@gmail.com>
2017-04-02 23:34:21 +0800

    loops

---
+++ loops.py
@@ -0,0 +1,19 @@
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+	print(name)
+
+
+sum = 0
+for x in [1, 2, 3, 4]:
+	sum += x
+print(sum)
+
+for x in list(range(10)):
+	print(x)
+
+sum = 0
+n = 99
+while n > 0:
+	sum += n
+	n -= 2
+print(sum)
