c8d257ff7d979eb1ce12645f60bab48d800d4a65
xcodej <xcoderj@gmail.com>
2017-04-04 20:41:19 +0800

    learn slides

---
+++ slides.py
@@ -0,0 +1,6 @@
+L = ['Michael', 'Sarah', 'Tracy', 'Bob', 'Jack']
+print('L[0: 3]', L[0: 3])
+# L[startIndex: endIndex: offset]
+# startIndex has a defaut value 0, and endIndex default is -1, the last element;
+print('L[:: -1]', L[:: -1])
+
