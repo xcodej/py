e88298c0df0f303bcc6d1ded3ed2896c8d13697b
xcodej <xcoderj@gmail.com>
2017-04-13 21:56:44 +0800

    primes

--- filter.py
+++ filter.py
@@ -20,3 +20,17 @@ for x in _odd_iter():
 
 def _not_divisible(n):
 	return lambda x: x % n > 0
+
+def primes():
+	yield 2
+	it = _odd_iter()
+	while True:
+		n = next(it)
+		yield n
+		it = filter(_not_divisible(n), it)
+
+for n in primes():
+	if n < 1000:
+		print(n)
+	else:
+		break
