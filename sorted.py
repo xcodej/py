4a312b46b8a48cdf9ec32c82714af62c86472684
xcodej <xcoderj@gmail.com>
2017-04-16 21:47:47 +0800

    sorted

--- sorted.py
+++ sorted.py
@@ -2,3 +2,12 @@ l = sorted([36, 5, -12, 9, -21])
 print(l)
 absL = sorted([36, 5, -12, 9, -21], key=abs)
 print(absL)
+strL = sorted(['bob', 'about', 'Zoo', 'Credit'])
+print(strL)
+
+lowerStrL = sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower)
+print(lowerStrL)
+
+reverseLowerStrL = sorted(['bob', 'about', 'Zoo', 'Credit'], key=str.lower, reverse=True)
+print(reverseLowerStrL)
+
