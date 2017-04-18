aef85adc7d6ece7f9bc7d2c7a5636dcc66dd0ec0
xcodej <xcoderj@gmail.com>
2017-04-18 23:27:13 +0800

    update

--- return_func.py
+++ return_func.py
@@ -4,3 +4,10 @@ def calc_sum(*args):
 		ax = ax + n
 	return ax
 
+def lazy_sum(*args):
+    def sum():
+        ax = 0
+        for n in args:
+            ax = ax + n
+        return ax
+    return sum
