c3404955f80cbead7cc61978e56e8b1c72de0688
xcodej <xcoderj@gmail.com>
2017-04-19 16:34:51 +0800

    closure

--- return_func.py
+++ return_func.py
@@ -11,3 +11,17 @@ def lazy_sum(*args):
             ax = ax + n
         return ax
     return sum
+
+f = lazy_sum(1, 3, 5, 7, 9)
+print(f)
+print(f())
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+
+f1, f2, f3 = count()
