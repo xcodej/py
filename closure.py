19f7282dcf86d2b3509b5ddbf9c8bab76af87a27
xcodej <xcoderj@gmail.com>
2017-04-21 22:20:05 +0800

    update

--- closure.py
+++ closure.py
@@ -5,3 +5,14 @@ def count():
              return i*i
         fs.append(f)
     return fs
+
+
+def count():
+    def f(j):
+        def g():
+            return j*j
+        return g
+    fs = []
+    for i in range(1, 4):
+        fs.append(f(i)) # f(i)立刻被执行，因此i的当前值被传入f()
+    return fs
