eda587f78c40956ad56444cddf54cc4ccd50902d
xcodej <xcoderj@gmail.com>
2017-04-28 20:52:11 +0800

    propertis

--- class_instance.py
+++ class_instance.py
@@ -2,4 +2,12 @@ class Student(object):
 	def __init__(self, name, score):
 		self.name = name
 		self.score = score
-bart = Student()
+# bart = Student()
+	def print_score(self):
+		print('%s: %s' % (self.name, self.score))
+
+bart = Student('Bart Simpson', 59)
+print(bart.name,bart.score)
+bart.print_score()
+bart.age = 10
+print(bart.age)
