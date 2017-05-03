cd9764264ddbb877bf5358a86855bdff6e4f2028
xcodej <xcoderj@gmail.com>
2017-05-04 00:15:35 +0800

    update

---
+++ extends.py
@@ -0,0 +1,23 @@
+class Animal(object):
+    pass
+
+# 大类:
+class Mammal(Animal):
+    pass
+
+class Bird(Animal):
+    pass
+
+# 各种动物:
+class Dog(Mammal):
+    pass
+
+class Bat(Mammal):
+    pass
+
+class Parrot(Bird):
+    pass
+
+class Ostrich(Bird):
+    pass
+
