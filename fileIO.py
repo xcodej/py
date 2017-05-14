2bd09d6cb1c5ae1482c53169c602d5ea0ed641c0
xcodej <xcoderj@gmail.com>
2017-05-14 20:32:03 +0800

    file write

--- fileIO.py
+++ fileIO.py
@@ -11,3 +11,10 @@ with open('text','r') as f:
 
 for line in f.readlines():
     print(line.strip()) # 把末尾的'\n'删掉
+
+try:
+	f - open('text', 'w')
+	f.write('Hello, world!')
+finally:
+	if f:
+		f.close()
