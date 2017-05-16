f681afe659d719acb4d5459443e1979a75af8702
xcodej <xcoderj@gmail.com>
2017-05-16 15:55:44 +0800

    update

--- StringIO.py
+++ StringIO.py
@@ -4,3 +4,13 @@ f.write('hello')
 f.write(' ')
 f.write('world!')
 print(f.getvalue())
+
+
+f = StringIO('Hello!\nHi!\nGoodbye!')
+while True:
+	s = f.readline()
+	if s == '':
+		break
+	print(s.strip())
+
+
