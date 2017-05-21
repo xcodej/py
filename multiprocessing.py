b6d85a5a7848446c869481bff6766d1cd8518125
xcodej <xcoderj@gmail.com>
2017-05-21 21:12:11 +0800

    update

--- multiprocessing.py
+++ multiprocessing.py
@@ -51,3 +51,17 @@ t2.start()
 t1.join()
 t2.join()
 print(balance)
+
+def process_student(name):
+    std = Student(name)
+    # std是局部变量，但是每个函数都要用它，因此必须传进去：
+    do_task_1(std)
+    do_task_2(std)
+
+def do_task_1(std):
+    do_subtask_1(std)
+    do_subtask_2(std)
+
+def do_task_2(std):
+    do_subtask_2(std)
+    do_subtask_2(std)
