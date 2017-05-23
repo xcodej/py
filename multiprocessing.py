80985e233fa81f4bffe6877d590b55b15e6aa254
xcodej <xcoderj@gmail.com>
2017-05-23 10:21:35 +0800

    update

--- multiprocessing.py
+++ multiprocessing.py
@@ -31,11 +31,9 @@ if __name__=='__main__':
 
 import time, threading
 
-# 假定这是你的银行存款:
 balance = 0
 
 def change_it(n):
-    # 先存后取，结果应该为0:
     global balance
     balance = balance + n
     balance = balance - n
@@ -54,7 +52,6 @@ print(balance)
 
 def process_student(name):
     std = Student(name)
-    # std是局部变量，但是每个函数都要用它，因此必须传进去：
     do_task_1(std)
     do_task_2(std)
 
@@ -69,35 +66,25 @@ def do_task_2(std):
 import random, time, queue
 from multiprocessing.managers import BaseManager
 
-# 发送任务的队列:
 task_queue = queue.Queue()
-# 接收结果的队列:
 result_queue = queue.Queue()
 
-# 从BaseManager继承的QueueManager:
 class QueueManager(BaseManager):
     pass
 
-# 把两个Queue都注册到网络上, callable参数关联了Queue对象:
 QueueManager.register('get_task_queue', callable=lambda: task_queue)
 QueueManager.register('get_result_queue', callable=lambda: result_queue)
-# 绑定端口5000, 设置验证码'abc':
 manager = QueueManager(address=('', 5000), authkey=b'abc')
-# 启动Queue:
 manager.start()
-# 获得通过网络访问的Queue对象:
 task = manager.get_task_queue()
 result = manager.get_result_queue()
-# 放几个任务进去:
 for i in range(10):
     n = random.randint(0, 10000)
     print('Put task %d...' % n)
     task.put(n)
-# 从result队列读取结果:
 print('Try get results...')
 for i in range(10):
     r = result.get(timeout=10)
     print('Result: %s' % r)
-# 关闭:
 manager.shutdown()
 print('master exit.')
