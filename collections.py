217a2d6317edbf59575f3d5c1391729c665f4769
xcodej <xcoderj@gmail.com>
2017-06-11 20:47:24 +0800

    Stack

--- collections.py
+++ collections.py
@@ -15,3 +15,21 @@ d = dict([('a', 1), ('b', 2), ('c', 3)])
 print(d)
 od = OrderedDict([('a', 1), ('b', 2), ('c', 3)])
 print(od)
+
+class LastUpdatedOrderedDict(OrderedDict):
+
+    def __init__(self, capacity):
+        super(LastUpdatedOrderedDict, self).__init__()
+        self._capacity = capacity
+
+    def __setitem__(self, key, value):
+        containsKey = 1 if key in self else 0
+        if len(self) - containsKey >= self._capacity:
+            last = self.popitem(last=False)
+            print('remove:', last)
+        if containsKey:
+            del self[key]
+            print('set:', (key, value))
+        else:
+            print('add:', (key, value))
+        OrderedDict.__setitem__(self, key, value)
