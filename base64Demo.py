ec4b3f9b1f6e996f1b588f3bed15fd3094e6d067
xcodej <xcoderj@gmail.com>
2017-06-20 15:10:32 +0800

    use safe url

--- base64Demo.py
+++ base64Demo.py
@@ -3,3 +3,10 @@ enString = base64.b64encode(b'binary\x00string')
 deString = base64.b64decode(enString)
 print(enString)
 print(deString)
+
+safeUrl = base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
+
+url = base64.urlsafe_b64decode('abcd--__')
+
+print(safeUrl)
+print(url)
